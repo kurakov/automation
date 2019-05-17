738588b97bf09c1371e1ffa62922f271282d07f8
Denys Kurakov <denyskurakov@gmail.com>
2019-05-16 20:49:45 -0400

    third day of practice

--- src/test/java/Day3.java
+++ src/test/java/Day3.java
@@ -79,7 +79,7 @@ public class Day3 {
 
     @Test
     public void testReverseString() {
-        String inputString = "MyValueValue";
+        String inputString = "MyValueValueValue";
 
         String reverse = "";
 
