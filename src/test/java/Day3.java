5a54547c42d1d5ce78792cd2661e463af427e0cc
Denys Kurakov <denyskurakov@gmail.com>
2019-05-16 20:58:48 -0400

    test

--- src/test/java/Day3.java
+++ src/test/java/Day3.java
@@ -79,7 +79,7 @@ public class Day3 {
 
     @Test
     public void testReverseString() {
-        String inputString = "MyValueValuevalue";
+        String inputString = "MyValueValue";
 
         String reverse = "";
 
