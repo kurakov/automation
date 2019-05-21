74588427cdccca2597cb0db9d0c10c8a2f0277cf
Denys Kurakov <denyskurakov@gmail.com>
2019-05-20 21:29:56 -0400

    day 4 firefox test pass

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,10 +20,24 @@ public class GoogleSearch {
         openMainPage();
         typeQuery();
         submitSerach();
+        waitForResults();
         assertResultPage();
 
     }
 
+    //TODO: automate search scenario on Bing.com
+    @Test
+    public void test002() {
+    }
+
+
+    private void waitForResults() {
+        By resultsStatsElement = By.id("resultStats");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(resultsStatsElement));
+    }
+
+    //TODO: ADD meaningful assertion
     private void assertResultPage() {
 
         WebElement resultsStatsElement = driver.findElement(By.id("resultStats"));
