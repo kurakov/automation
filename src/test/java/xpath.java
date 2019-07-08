9d68e46b60b2fc4c9e19029f56a7800473474fd8
Denys Kurakov <denyskurakov@gmail.com>
2019-07-08 18:30:34 -0400

    xpath 13 methods

--- src/test/java/xpath.java
+++ src/test/java/xpath.java
@@ -21,6 +21,16 @@ public class xpath {
         String adsXpath = "html/body/div[1]/div[2]/div[1]/form/div[1]/div/div[1]/div/div/input[1]";
         String relativeXpath = "//form/div[1]/div/div[1]/div/div/input[1]";
         String xpath2 = "//input[@name='restaurant_id']";
+        String xpath3 = "//input[@name='restaurant_id'][@type='text']";
+        String xpath4 = "//input[@name='restaurant_id' and @type='text']";
+        String xpath5 = "//input[@name='restaurant_id' or @type='text']";
+        String xpath6 = "//input[contains(@name,'taurant'";
+        String xpath7 = "//input[starts-with(@name, 'resta']";
+        String xpath8 = "//*[text()='Log In']";//is.Displayed()
+        String xpath9 = "(//input[@type='text'])[last()]";
+        String xpath10 = "(//input[@type='text'])[position()=2]";
+        String xpath11 = "(//input[@type='text'])[2]";
+//        String xpath12 = "//*[@id='FirstName']/following::input[@type='text']";
+//        String xpath13 = "//*[@id='LastName']//preceding::input[@type='text']";
     }
 }
-//aa
